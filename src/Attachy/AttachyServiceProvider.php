3f9e30051c8239144c95864c8246baccd4ca71ec
Etherealite <evanbanghm@yahoo.com>
2012-11-29 11:21:07 -0800

    setting up stuff for L4

---
+++ src/Attachy/AttachyServiceProvider.php
@@ -0,0 +1,8 @@
+<?php namespace Attachy;
+
+use Illuminate\Support\ServiceProvider;
+
+class AttachyServiceProvider extends ServiceProvider {
+
+}
+
---
+++ src/Attachy/Facades/Attachy.php
@@ -0,0 +1,14 @@
+<?php Attachy\Facades;
+
+use Illuminate\Support\Facade;
+
+class Attachy extends Facade {
+
+  /**
+  * Get the registered component.
+  *
+  * @return string
+  */
+  protected static function getFacadeAccessor(){ return 'attachy'; }
+
+}
